975893e2c67effbe831b5efaea8d12b562685146
fayyumqa <169048855+fayyumqa@users.noreply.github.com>
2024-05-30 15:57:21 +0530

    Add files via upload

---
+++ Basic 1.0 - HW.py
@@ -0,0 +1,25 @@
+import keyword
+
+print("Lokesh","Neman","Zula",sep="\n",end="\t")
+print("Lokesh","Neman","Zula")
+print(keyword.kwlist)
+name1="Wakes"
+name= 3
+print(type(name))
+print(type(name1))
+print(name,name1)
+cool="Fayyum"
+print(type(cool))
+pi=3.14
+print(type(pi))
+CN=3+4j
+print(type(CN))
+male=True
+print(type(male))
+firstname=input("Enter Your First Name")
+print(firstname)
+middletname=input("Enter Your Middle Name")
+print(middletname)
+lastname=input("Enter Your Last Name")
+print(lastname)
+print(firstname,middletname,lastname)
