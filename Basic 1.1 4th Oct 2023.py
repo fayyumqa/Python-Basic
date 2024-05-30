217a3143f607707bf60a3f3561f7ecd1048646e7
fayyumqa <169048855+fayyumqa@users.noreply.github.com>
2024-05-30 15:57:50 +0530

    Add files via upload

---
+++ Basic 1.1 4th Oct 2023.py
@@ -0,0 +1,14 @@
+#list=max(1,2,3,1,22,34)
+#print(list)
+list=min(1,2,3,4,44)
+print(list,"Fatyyum")
+classy="fasyyum,"
+print(classy)
+name="Fayyum"
+print(name)
+name1="Shaikh"
+print(name)
+male=False
+print(type(male))
+name=input("enter your name")
+print("welcome to the class",name)
